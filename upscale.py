09a9425027b294b98932b50f6aeb78632bc21ef8
reddyn12 <nikhilr.ssm@gmail.com>
2023-06-29 17:00:38 -0400

    working ont it

--- upscale.py
+++ upscale.py
@@ -28,30 +28,34 @@ print(imageArr[20:25, 20:25])
 
 
 def upscalerSimpleRepeat(image):
+    iHeight = 32
+    iWidth = 32
     width = 63
     height = 63
-    ans = np.zeros(shape=(width, height))
+    ans = np.zeros(shape=(height, iWidth), dtype=np.uint8)
 
-    for h in height:
-        for w in width:
-            nH = None
-            nW = None
-            if h%2==0:
-                nH = h
-            if w%2==0:
-                nW = w
-            if(nW is None and nH is None):
-                pass
-            elif(nW is None):
-                pass
-            elif(nH is None):
-                pass
-            else:
-                ans[nH]
-
-    print(type(ans), ans.shape)
-    print(type(image), image.shape)
-
+    # currInd = 1
+    # imgInd = 0
+    # print(type(ans), ans.shape)
+    # print(type(image), image.shape)
+    # ans[0] = image[0]
+    # imgInd = 1
+    # while currInd < height-1:
+        
+    #     ans[currInd] = (image[imgInd-1]+image[imgInd])/2
+    #     print("VALS: ", image[imgInd-1], image[imgInd], ans[currInd])
+    #     currInd= currInd+2
+    #     ans[currInd-1] = image[imgInd]
+    #     imgInd = imgInd+1
+    
+    for i in range(iHeight-1):
+        ans[i*2] = image[i]
+        ans[i*2+1] = (image[i]+image[i+1])/2
+        print(ans[i*2+1, 10:20])
+        
+    # ans[-1] = image[-1]
+    
+    return ans
 
 
 
@@ -63,4 +67,7 @@ def normalizeHelper(image):
     
     return ans
 
-upscaler(imageArr)
+arr = upscalerSimpleRepeat(imageArr)
+newImg = Image.fromarray(arr)
+newImg.show()
+IMG.show()
